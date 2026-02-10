75e3b1eb4cfe750efa4e11449fef2e21ee7dd825
Jackie <jackie.blaum@gmail.com>
2026-02-09 22:38:12 -0800

    test: add test for wavelength-dependent extinction
    Add test_extinction_wavelength_dependent to verify that F99 extinction
    is stronger at shorter (bluer) wavelengths than at longer (redder)
    wavelengths. This test currently fails as expected since extinction
    has not been implemented yet.

--- tests/test_sed_extinction.py
+++ tests/test_sed_extinction.py
@@ -43,3 +43,32 @@ def test_extinction_reduces_flux():
 
     # Extinction should not increase flux
     assert np.all(f_yes <= f_no), "Extinction should reduce flux for all filters"
+
+
+def test_extinction_wavelength_dependent():
+    """Extinction should be stronger at shorter wavelengths"""
+    wrapper_no = _make_wrapper(ebv=0.0)
+    wrapper_yes = _make_wrapper(ebv=0.2)
+
+    res_no = wrapper_no.compute_sed(phases=[0.25])
+    res_yes = wrapper_yes.compute_sed(phases=[0.25])
+
+    wl = res_no["wavelengths"]
+    f_no = res_no["fluxes"][0]
+    f_yes = res_yes["fluxes"][0]
+
+    mask = np.isfinite(wl) & np.isfinite(f_no) & np.isfinite(f_yes)
+    wl = wl[mask]
+    f_no = f_no[mask]
+    f_yes = f_yes[mask]
+
+    # Find bluest and reddest filters
+    blue_idx = np.argmin(wl)
+    red_idx = np.argmax(wl)
+
+    # Compute attenuation ratios
+    blue_ratio = f_yes[blue_idx] / f_no[blue_idx]
+    red_ratio = f_yes[red_idx] / f_no[red_idx]
+
+    # Blue should be more attenuated (smaller ratio)
+    assert blue_ratio < red_ratio, "Blue filters should have stronger extinction"
